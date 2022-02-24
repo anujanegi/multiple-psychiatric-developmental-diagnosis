15e2e8f1e7319e266f08a9c8880044ac1584eea3
anujanegi <anujanegi1@gmail.com>
2022-02-24 18:46:33 +0100

    eval script

--- evaluation_script/main.py
+++ evaluation_script/main.py
@@ -1,81 +1,81 @@
-import random
-
+import pandas as pd
+import numpy as np
+from sklearn.metrics import average_precision_score, roc_curve, auc, hamming_loss, classification_report
 
 def evaluate(test_annotation_file, user_submission_file, phase_codename, **kwargs):
     print("Starting Evaluation.....")
-    """
-    Evaluates the submission for a particular challenge phase and returns score
-    Arguments:
 
-        `test_annotations_file`: Path to test_annotation_file on the server
-        `user_submission_file`: Path to file submitted by the user
-        `phase_codename`: Phase to which submission is made
+    print(kwargs["submission_metadata"])
+    y_true = np.array(pd.read_csv(test_annotation_file).drop('IDs', axis=1))
+    y_pred = np.array(pd.read_csv(user_submission_file, header=None))
 
-        `**kwargs`: keyword arguments that contains additional submission
-        metadata that challenge hosts can use to send slack notification.
-        You can access the submission metadata
-        with kwargs['submission_metadata']
+    try:
+        assert(y_true.shape == y_pred.shape)
+    except:
+        raise ValueError("Submitted data doesn't match the required shape of %s"%str(y_true.shape))
+    
+    auprc = average_precision_score(y_true, y_pred, average="macro")
+    
+    n_classes = y_true.shape[1]
+    fpr = dict()
+    tpr = dict()
 
-        Example: A sample submission metadata can be accessed like this:
-        >>> print(kwargs['submission_metadata'])
-        {
-            'status': u'running',
-            'when_made_public': None,
-            'participant_team': 5,
-            'input_file': 'https://abc.xyz/path/to/submission/file.json',
-            'execution_time': u'123',
-            'publication_url': u'ABC',
-            'challenge_phase': 1,
-            'created_by': u'ABC',
-            'stdout_file': 'https://abc.xyz/path/to/stdout/file.json',
-            'method_name': u'Test',
-            'stderr_file': 'https://abc.xyz/path/to/stderr/file.json',
-            'participant_team_name': u'Test Team',
-            'project_url': u'http://foo.bar',
-            'method_description': u'ABC',
-            'is_public': False,
-            'submission_result_file': 'https://abc.xyz/path/result/file.json',
-            'id': 123,
-            'submitted_at': u'2017-03-20T19:22:03.880652Z'
-        }
-    """
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = roc_curve(y_true[:, i], y_pred[:, i])
+    # Compute macro-average ROC curve and ROC area
+
+    # First aggregate all false positive rates
+    all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+
+    # Then interpolate all ROC curves at this points
+    mean_tpr = np.zeros_like(all_fpr)
+    for i in range(n_classes):
+        mean_tpr += np.interp(all_fpr, fpr[i], tpr[i])
+
+    # Finally average it and compute AUC
+    mean_tpr /= n_classes
+    auroc = auc(all_fpr, mean_tpr)
+    
+    brier_score = np.mean(np.mean((y_pred - y_true)**2, axis=1))
+    hamming_score = hamming_loss(y_true, y_pred.round())
+    
+    report = classification_report(y_true, y_pred.round(), zero_division=1, output_dict=True)
+    micro_avg_f1_score = report['micro avg']['f1-score']
+    
     output = {}
-    if phase_codename == "dev":
-        print("Evaluating for Dev Phase")
+    if phase_codename == "pseudo-test":
+        print("Evaluating for Pseudo Test Phase")
         output["result"] = [
             {
-                "train_split": {
-                    "Metric1": random.randint(0, 99),
-                    "Metric2": random.randint(0, 99),
-                    "Metric3": random.randint(0, 99),
-                    "Total": random.randint(0, 99),
+                "test_split": {
+                    "AUPRC": auprc,
+                    "AUROC": auroc,
+                    "Brier Score": brier_score,
+                    "Hamming Score": hamming_score,
+                    "Micro Avg F1 score": micro_avg_f1_score
                 }
             }
         ]
         # To display the results in the result file
-        output["submission_result"] = output["result"][0]["train_split"]
-        print("Completed evaluation for Dev Phase")
-    elif phase_codename == "test":
-        print("Evaluating for Test Phase")
+        output["submission_result"] = output["result"][0]["test_split"]
+        print("Completed evaluation for Pseudo Test Phase")
+        
+    elif phase_codename == "dev":
+        print("Evaluating for Dev Phase")
         output["result"] = [
             {
-                "train_split": {
-                    "Metric1": random.randint(0, 99),
-                    "Metric2": random.randint(0, 99),
-                    "Metric3": random.randint(0, 99),
-                    "Total": random.randint(0, 99),
-                }
-            },
-            {
                 "test_split": {
-                    "Metric1": random.randint(0, 99),
-                    "Metric2": random.randint(0, 99),
-                    "Metric3": random.randint(0, 99),
-                    "Total": random.randint(0, 99),
+                    "AUPRC": auprc,
+                    "AUROC": auroc,
+                    "Brier Score": brier_score,
+                    "Hamming Score": hamming_score,
+                    "Micro Avg F1 score": micro_avg_f1_score
                 }
-            },
+            }
         ]
         # To display the results in the result file
-        output["submission_result"] = output["result"][0]
-        print("Completed evaluation for Test Phase")
+        output["submission_result"] = output["result"][0]["test_split"]
+        print("Completed evaluation for Dev Phase")
+        
+    print(output)
     return output
